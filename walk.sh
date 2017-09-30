3ab82bb53ae516cefe710a32ed2d14f938e5d74f
Tristan Glatard <tristan.glatard@concordia.ca>
2017-09-29 21:50:04 -0400

    Put tools in 'tools'

--- walk.sh
+++ walk.sh
@@ -7,8 +7,8 @@ files=$(find . -name '*.json' -type f)
 
 for file in ${files}
 do
-    ln -s ${file}
-    git add $(basename ${file})
+    name = tools/$(basename ${files})
+    test -h ${name} || (ln -s ${file} ${name} && git add ${name})
 done
 
 git commit -m "Walked the repository for new files in submodules"
