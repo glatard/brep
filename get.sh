f4ff8656f98550cff3991036858c2989adf2d86c
Tristan Glatard <tristan.glatard@concordia.ca>
2017-09-29 23:01:10 -0400

    Fixed bug

--- get.sh
+++ get.sh
@@ -6,7 +6,7 @@ set -u
 file=$1
 
 link=$(file "${file}" | awk '{print $NF}')
-submodule=$(echo "${link}" | awk -F '/' '{print $1"/"$2"/"$3}')
+submodule=$(echo "${link}" | awk -F '/' '{print $2"/"$3"/"$4}')
 
 git submodule init
 git submodule update "${submodule}"
