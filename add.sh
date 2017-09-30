484e99e02b9daf4d05fa489b9a347880b1a66501
Tristan Glatard <tristan.glatard@concordia.ca>
2017-09-29 22:55:15 -0400

    Shellchecked scripts

--- add.sh
+++ add.sh
@@ -3,13 +3,15 @@
 set -e
 set -u
 
-git submodule add $1 ./submodules/$(basename $1)
+URL="$1"
+
+git submodule add "${URL}" ./submodules/"$(basename "${URL}")"
 
 files=$(find . -name '*.json' -type f)
 for file in ${files}
 do
-    name=tools/$(basename ${file})
-    test -h ${name} || (ln -s ../${file} ${name} && git add ${name})
+    name=tools/"$(basename "${file}")"
+    test -h "${name}" || (ln -s ../"${file}" "${name}" && git add "${name}")
 done
 
 git commit -m "Added new submodule $1"
--- get.sh
+++ get.sh
@@ -5,8 +5,8 @@ set -u
 
 file=$1
 
-link=$(file ${file} | awk '{print $NF}')
-submodule=$(echo ${link} | awk -F '/' '{print $1"/"$2"/"$3}')
+link=$(file "${file}" | awk '{print $NF}')
+submodule=$(echo "${link}" | awk -F '/' '{print $1"/"$2"/"$3}')
 
 git submodule init
-git submodule update ${submodule}
+git submodule update "${submodule}"
